be9fb9071683803f33fa9b9fc1c9d5ac779f90d5
Sablin-Alexey <lordsabre89@gmail.com>
2023-01-12 22:23:10 +0300

    First

---
+++ src/main/java/Method.java
@@ -0,0 +1,21 @@
+
+
+public enum Method {
+    GET("GET"),
+    POST("POST"),
+    DELETE("DELETE");
+
+    private String name;
+
+    Method(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
